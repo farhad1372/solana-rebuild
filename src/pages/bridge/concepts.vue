9d3dc773778f4d3be688f6ed42fdcb3509199058
farhad1372 <farhadfallahpour.eleceng@gmail.com>
2023-01-09 17:24:52 +0330

    conceps done

--- src/pages/bridge/concepts.vue
+++ src/pages/bridge/concepts.vue
@@ -11,37 +11,37 @@
         synthetic derivatives that represent an asset from another blockchain.<br />
         If you use a bridge to send one Solana coin to an Ethereum wallet, that wallet
         will receive a token that has been “wrapped” by the bridge converted to a token
-        based on the target <br />
+        based on the target
         blockchain. In this case, the Ethereum wallet would receive a "bridge" version of
         Solana that has been converted to an ERC-20 token the generic token standard for
-        fungible tokens on <br />
+        fungible tokens on
         the Ethereum blockchain. <br />
         While bridges open up new markets and work toward a brighter multi-chain future,
         they come with their own security challenges, as proven by a huge $326 million
-        exploit on the nascent <br />
+        exploit on the nascent
         Wormhole bridge in February 2022.<br />
       </p>
       <h2 class="mt-10">Types of blockchain bridges</h2>
       <p>
         Some bridges, known as unidirectional or one-way bridges, allow you to port assets
         only to the target blockchain and not the other way around. For instance, Wrapped
-        Bitcoin allows you<br />
+        Bitcoin allows you
         to send bitcoin to the Ethereum blockchain to convert BTC to an ERC-20 stablecoin
         but it doesn't let you send ether to the Bitcoin blockchain.<br />
         Other bridges like Wormhole and Multichain are bidirectional, or two-way, meaning
         you can freely convert assets to and from blockchains. Just as you can send Solana
-        to Ethereum's <br />
+        to Ethereum's
         blockchain, you can send ether to Solana.<br />
         Bridges are either custodial (also known as centralized or trusted) or
         noncustodial (decentralized or trustless). The difference explains who controls
-        the tokens that are used to create the <br />
+        the tokens that are used to create the
         bridged assets. All wrapped bitcoin (WBTC) is held in custody by BitGo, making it
         a centralized bridge. Conversely, bridged assets on Wormhole are held by the
-        protocol, meaning it is <br />
-        more decentralized.<br />
+        protocol, meaning it is
+        more decentralized.
         While hardline advocates of decentralization might venture that the custodial
         nature of WBTC makes it less secure than decentralized alternatives, bridges that
-        decentralize custody over<br />
+        decentralize custody over
         bridged assets aren't necessarily safer, as shown by the Wormhole bridge
         exploit.<br />
       </p>
@@ -49,21 +49,21 @@
       <p>
         Porting assets from one blockchain to another blockchain comes with a myriad of
         benefits. First, the blockchain onto which you port assets might be cheaper and
-        faster than its native <br />
+        faster than its native
         blockchain. This is certainly true for Ethereum, where high transaction fees and
         slow throughput make it difficult for newcomers to get involved in decentralized
         finance (DeFi).<br />
         If investors ported assets to a layer 2 network a faster blockchain that sits atop
         the Ethereum blockchain, like Arbitrum or Polygon they could trade ERC-20 tokens
-        for a fraction of the<br />
+        for a fraction of the
         cost without sacrificing exposure to Ethereum tokens.<br />
         Other investors might use bridges to make the most of markets that exist only on
         another blockchain. For instance, the DeFi protocol Orca is available only on
-        Solana, but supports a <br />
+        Solana, but supports a
         wrapped version of ETH.<br />
         Bridges are becoming easier to use. Many DeFi protocols have integrated bridges to
         let their users swap tokens from different protocols without having to leave the
-        platform. This makes <br />
+        platform. This makes
         the process of converting tokens through bridges less cumbersome.<br />
       </p>
 
@@ -71,12 +71,12 @@
       <p>
         According to DeFi Llama, there was $21.8 billion worth of crypto locked in bridges
         as of March 2022. The largest blockchain bridge is Wrapped Bitcoin, accounting for
-        almost half of the <br />
+        almost half of the
         bridge market, with $10.2 billion in total value locked (TVL). DeFi Llama pegs
         Multichain as the largest cross-chain bridge, with about $7 billion in TVL.<br />
         A dashboard on Dune Analytics shows that the Avalanche Bridge is the largest
         Ethereum bridge, with about $6 billion in TVL, followed by Polygon ($5 billion
-        TVL) and the Fantom <br />
+        TVL) and the Fantom
         Anyswap Bridge ($4.2 billion TVL).<br />
       </p>
     </div>
--- src/pages/nft/concepts.vue
+++ src/pages/nft/concepts.vue
@@ -3,7 +3,7 @@
     <div calss="col-12">
       <img class="rounded" src="@/assets/images/NFT.jpg" style="width: 100%" />
       <h2 class="mt-10">What is NFT?</h2>
-      <p>
+      <p class="mb-3">
         NFT means non-fungible tokens(NFTs), which are generally created using the same
         type of programming used for cryptocurrencies. In simple terms these cryptographic
         assets are<br />
@@ -27,30 +27,36 @@
         <li>Unique - It cannot be forged or otherwise manipulated.</li>
         <li>Exchange - NFT exchanges take place with cryptocurrencies such as Bitcoin on specialist sites.</li>
       </ul>
-     <h2 class="mt-10">How Does NFT Work?</h2>
-   <p>Now that you've taken your initial steps in understanding what an NFT is, you should continue on and learn about how an NFT works.</p>
-   <ul>
-   <li>The majority of NFTs reside on the Ethereum cryptocurrency's blockchain, a distributed public ledger that records transactions.  </li>
-   <li>NFTs are individual tokens with valuable information stored in them.  </li>
-   <li>Because they hold a value primarily set by the market and demand, they can be bought and sold just like other physical types of ar  </li>
-   <li>NFTs' unique data makes it easy to verify and validate their ownership and the transfer of tokens between owners.  </li>
-   </ul>
-  <h2 class="mt-10">Examples of NFT</h2>
-  <p>The NFT world is relatively new to people. Here are some examples of NFTs that exist today:</p>
-  <ul>
-  <li>A Digital Collectible</li>
-  <li>Domain Names</li>
-  <li>Games</li>
-  <li>Essays</li>
-  <li>Sneakers in fashion line</li>
-  </ul>
-<h2 class="mt-10">What is NFT Used For?</h2>
-<p>People interested in Crypto-trading and people who like to collect artwork often use NFTs. Other than that, it has some other uses too like:</p>
-<ul>
-<li>Digital Content - The most significant use of NFTs today is in digital content. Content creators see their profits enhanced by NFTs, as they power a creator economy where creators <br />
-have the ownership of their content over to the platforms they use to publicize it.
-</li>
-</ul>
+      <h2 class="mt-16">How Does NFT Work?</h2>
+      <p class="mb-4">Now that you've taken your initial steps in understanding what an NFT is, you should continue on
+        and learn about how an NFT works.</p>
+      <ul>
+        <li>The majority of NFTs reside on the Ethereum cryptocurrency's blockchain, a distributed public ledger that
+          records transactions. </li>
+        <li>NFTs are individual tokens with valuable information stored in them. </li>
+        <li>Because they hold a value primarily set by the market and demand, they can be bought and sold just like
+          other physical types of ar </li>
+        <li>NFTs' unique data makes it easy to verify and validate their ownership and the transfer of tokens between
+          owners. </li>
+      </ul>
+      <h2 class="mt-16">Examples of NFT</h2>
+      <p class="mb-4">The NFT world is relatively new to people. Here are some examples of NFTs that exist today:</p>
+      <ul>
+        <li>A Digital Collectible</li>
+        <li>Domain Names</li>
+        <li>Games</li>
+        <li>Essays</li>
+        <li>Sneakers in fashion line</li>
+      </ul>
+      <h2 class="mt-16">What is NFT Used For?</h2>
+      <p class="mb-4">People interested in Crypto-trading and people who like to collect artwork often use NFTs. Other than that, it
+        has some other uses too like:</p>
+      <ul>
+        <li>Digital Content - The most significant use of NFTs today is in digital content. Content creators see their
+          profits enhanced by NFTs, as they power a creator economy where creators <br />
+          have the ownership of their content over to the platforms they use to publicize it.
+        </li>
+      </ul>
     </div>
   </div>
 </template>
